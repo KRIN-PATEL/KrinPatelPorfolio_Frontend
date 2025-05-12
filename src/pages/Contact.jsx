fe9ffdb05b985251737481ca83f1ba427e61af45
KRIN-PATEL <190770116089@socet.edu.in>
2025-05-12 15:54:04 -0400

    new contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -99,7 +99,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/send", {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/send`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
